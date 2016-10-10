85a62222b5addf6a2f06ab34cc292c51ab15a8aa
David Feldbaum <Dfeldbaum@Davids-MacBook-Pro-2.local>
2016-10-10 01:55:32 -0500

    added milk cartons object

--- public/javascripts/newapp.js
+++ public/javascripts/newapp.js
@@ -2,19 +2,21 @@
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamediv', { preload: preload, create: create, update: update }); // empty string is id for canvas el
 
+var player;
+var cursors;
+var milkCartons;
+var score = 0;
+
 function preload() {
 
     game.load.image('wood', 'images/wood.png');
+    game.load.image('milk', 'images/milkcarton.png');
     game.load.spritesheet('ronnie', 'images/ronniespritesheet2.png', 33, 48);
-    // game.load.image('platform', 'images/platform.png');
-    // game.load.image('star', 'images/star.png');
     
 }
 
-
 function create() {
-    /// Create Game World
-
+    // Create Game World
     // Add physics engine
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -22,33 +24,41 @@ function create() {
     game.world.setBounds(0, 0, 800, 600);
     background = game.add.tileSprite(0, 0, 800, 600, 'wood');
 
-
-    // Create Ronnie Player 
+    // Create Ronnie player 
     player = game.add.sprite(375, 275, 'ronnie');
     game.physics.arcade.enable(player);
     player.body.collideWorldBounds = true;
-    player.animations.add('left', [1], 6, true);
-    player.animations.add('right', [2],6, true);
-    player.animations.add('up', [0], 6, true);
-    player.animations.add('down', [0], 6, true);
+    player.animations.add('left', [1], 10, true);
+    player.animations.add('right', [2],10, true);
+    player.animations.add('up', [0], 10, true);
+    player.animations.add('down', [0], 10, true);
+
+    // Create Milk objects in a group
+    milks = game.add.group();
+    milks.enableBody = true;
+    // Create milk cartons
+    for (var i = 0; i < 50; i++) {
+    	// create the actual star
+        var milk = milks.create(i * 35, 0, 'milk');
+    }
 
     // Add score text
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' }); 
+    scoreText = game.add.text(15, 15, 'score: 0', { fontSize: '30px', fill: '#000' }); 
 
-    /// Controls /// 
+    // Controls 
     cursors = game.input.keyboard.createCursorKeys();
-
 
 }    
 
 function update() {
-
-    // Reset player movement (left/right)
-
+	// Run drinkMilk function with player, milk collisions
+	game.physics.arcade.overlap(player, milks, drinkMilk, null, this);
+    
+    // Reset player movement (left/right/up/down)
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
 
-    /// Connect controls to Ronnie player ///
+    // Connect left/right controls to Ronnie player 
     if (cursors.left.isDown) {
         // move player left with 150 speed
         player.body.velocity.x = -150;
@@ -66,13 +76,14 @@ function update() {
         player.frame = 4;
     }
 
-
-
+    // Connect up/down controls to Ronnie player
     if (cursors.up.isDown) {
+    	// move player up with 150 speed
     	player.body.velocity.y = -150;
     	player.animations.play('up'); 
     }    
     else if (cursors.down.isDown) {
+    	// move player down with 150 speed
     	player.body.velocity.y = 150;
     	player.animations.play('down');
     }
@@ -83,14 +94,13 @@ function update() {
     }
 
 
-
-    // // Connect cursors var with player for jump
-    // if (cursors.up.isDown && player.body.touching.down) {
-    //     player.body.velocity.y = -350;
-    // } 
+    function drinkMilk (player, milk) {
+    	milk.kill();
+    	score += 5;
+    	scoreText.text = 'Score: ' + score;
+    }
 
 }
-
 
 
 
