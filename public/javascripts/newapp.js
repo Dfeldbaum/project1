502ddf2e2b340d591df3666b5948f0f481b4bf1c
David Feldbaum <Dfeldbaum@Davids-MacBook-Pro-2.local>
2016-10-10 19:01:15 -0500

    added abbench and benches obstacles

--- public/javascripts/newapp.js
+++ public/javascripts/newapp.js
@@ -13,6 +13,11 @@ function preload() {
     game.load.image('milk', 'images/milkcarton.png');
     game.load.image('treadmill-up', 'images/treadmill-up-80.png');
     game.load.image('treadmill-down', 'images/treadmill-down-80.png');
+    game.load.image('yogamat', 'images/yoga-mat-65.png');
+    game.load.image('gymball', 'images/gymball-30.png');
+    game.load.image('eliptical', 'images/eliptical-70.png');
+    game.load.image('abbench', 'images/abbench-70.png');
+    game.load.image('bench', 'images/bench-70.png');
     game.load.spritesheet('ronnie', 'images/ronniespritesheet2.png', 33, 48);
     
 }
@@ -39,12 +44,12 @@ function create() {
     // milks = game.add.group();
     // milks.enableBody = true;
     // // Create milk cartons
-    // for (var i = 0; i < 50; i++) {
+    // for (var i = 0; i < 10; i++) {
     // 	// create the actual star
-    //     var milk = milks.create(i * 35, 700, 'milk');
+    //     var milk = milks.create(i * 50, 400, 'milk');
     // }
 
-    // Create treadmill-up obstacle group
+    // Create treadmill obstacle group
 
     treadmills = game.add.group();
     treadmills.enableBody = true;
@@ -72,7 +77,6 @@ function create() {
 
    	treadmillUp = treadmills.create(600, 5, 'treadmill-up');
    	treadmillUp.body.immovable = true;
-
 
 	treadmillUp = treadmills.create(650, 5, 'treadmill-up');
    	treadmillUp.body.immovable = true;
@@ -117,10 +121,88 @@ function create() {
    	treadmillDown = treadmills.create(515, 515, 'treadmill-down');
    	treadmillDown.body.immovable = true;
 
+   	// Create yoga mat obstacle group
+   	yogamats = game.add.group();
+    yogamats.enableBody = true;
+
+    var yogamat = yogamats.create(650, 563, 'yogamat');
+    yogamat.body.immovable = true;
+
+   	yogamat = yogamats.create(650, 533, 'yogamat');
+   	yogamat.body.immovable = true;
+
+   	// Create gymball obstacle group, physics
+   	gymballs = game.add.group();
+    gymballs.enableBody = true;
+
+    var gymball = gymballs.create(740, 559, 'gymball');
+    gymball.body.immovable = false;
+    gymball.body.collideWorldBounds = true;
+    gymball.body.bounce.y = 0.2;
+    gymball.body.bounce.x = 0.2;
+
+   	gymball = gymballs.create(740, 527, 'gymball');
+   	gymball.body.immovable = false;
+   	gymball.body.collideWorldBounds = true;
+   	gymball.body.bounce.y = 0.2;
+    gymball.body.bounce.x = 0.2;
+
+   	// var gymball = gymballs.create(760, 557, 'gymball');
+    // gymball.body.immovable = false;
+    // gymball.body.collideWorldBounds = true;
+    // gymball.body.bounce.y = 0.2;
+    // gymball.body.bounce.x = 0.2;
+
+   	// gymball = gymballs.create(760, 525, 'gymball');
+   	// gymball.body.immovable = false;
+   	// gymball.body.collideWorldBounds = true;
+   	// gymball.body.bounce.y = 0.2;
+   	// gymball.body.bounce.x = 0.2;
+
+   	// Create eliptical group
+   	elipticals = game.add.group();
+    elipticals.enableBody = true;
+
+
+   	var eliptical = elipticals.create(720, 210, 'eliptical');
+    eliptical.body.immovable = true;
+
+    eliptical = elipticals.create(720, 260, 'eliptical');
+   	eliptical.body.immovable = true;
+
+   	eliptical = elipticals.create(720, 310, 'eliptical');
+   	eliptical.body.immovable = true;
+
+   	eliptical = elipticals.create(720, 360, 'eliptical');
+   	eliptical.body.immovable = true;
+
+
+	// Create ab bench
+	abbenches = game.add.group();
+    abbenches.enableBody = true;
+
+   	var abbench = abbenches.create(10, 210, 'abbench');
+    abbench.body.immovable = true;
+
+
+	// Create bench group
+   	benches = game.add.group();
+    benches.enableBody = true;
+
+   	var bench = benches.create(10, 250, 'bench');
+    bench.body.immovable = true;
+
+   	bench = benches.create(10, 300, 'bench');
+   	bench.body.immovable = true;
+
+   	bench = benches.create(10, 350, 'bench');
+   	bench.body.immovable = true;
+
+
 
 
     // Add score text
-    scoreText = game.add.text(12, 12, 'protein pts: 0', { fontSize: '25px', fill: '#000' }); 
+    scoreText = game.add.text(25, 15, 'protein pts: 0', { fontSize: '28px', fill: '#000' }); 
 
     // Controls 
     cursors = game.input.keyboard.createCursorKeys();
@@ -128,12 +210,26 @@ function create() {
 }    
 
 function update() {
-	// Create seperation with Ronnie, treadmill group collisions
+
+	// Create seperation with collisions
 	game.physics.arcade.collide(player, treadmills);
+	// game.physics.arcade.collide(player, yogamats);
+	game.physics.arcade.collide(player, gymballs);
+	game.physics.arcade.collide(treadmills, gymballs);
+	game.physics.arcade.collide(gymballs, gymballs);
+	game.physics.arcade.collide(player, elipticals);
+	game.physics.arcade.collide(elipticals, gymballs);
+	game.physics.arcade.collide(player, abbenches);
+	game.physics.arcade.collide(abbenches, gymballs);
+	game.physics.arcade.collide(player, benches);
+	game.physics.arcade.collide(benches, gymballs);
 
 
-	// Run drinkMilk function with player, milk collisions
+
+	// Create overlap collision physics
 	// game.physics.arcade.overlap(player, milks, drinkMilk, null, this);
+	// game.physics.arcade.overlap(player, yogamats, null, null, this);
+
     
     // Reset player movement (left/right/up/down)
     player.body.velocity.x = 0;
@@ -175,11 +271,11 @@ function update() {
     }
 
 
-    // function drinkMilk (player, milk) {
-    // 	milk.kill();
-    // 	score += 5;
-    // 	scoreText.text = 'protein pts: ' + score;
-    // }
+//     function drinkMilk (player, milk) {
+//     	milk.kill();
+//     	score += 5;
+//     	scoreText.text = 'protein pts: ' + score;
+//     }
 
 }
 
